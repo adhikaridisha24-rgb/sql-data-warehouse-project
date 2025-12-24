d9d74f909c99ddb79c84b20479fc05214a726e7c
adhikaridisha24-rgb <adhikaridisha24@gmail.com>
2025-12-24 17:20:15 +0530

    Delete scripts/load_data.sql

--- scripts/load_data.sql
+++
@@ -1,8 +0,0 @@
-LOAD DATA LOCAL INFILE 'C:\Users\disha\Downloads\f78e076e5b83435d84c6b6af75d8a679\sql-data-warehouse-project\datasets\source_crm\cust_info.csv'
-INTO TABLE bronze.crm_cust_info
-FIELDS TERMINATED BY ','
-LINES TERMINATED BY '\n'
-IGNORE 1 ROWS;
-
-SHOW VARIABLES LIKE 'local_infile';
-SET GLOBAL local_infile = 1;
